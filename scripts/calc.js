3a12eed456321599f9cc745ec9a845b558f41c77
Kitty McDonagh <kittymcdonagh@gmail.com>
2019-05-24 08:49:56 +0000

    Drink About Challenge added

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,8 +1,31 @@
-function addition(numOne, numTwo) {
-    if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo;
-    } else {
-        alert("Error!");
-    }
-    
+Calculator = function() {
+    this.value = 0;
 }
+
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
+    }
+    else
+        alert("Error!");
+
+}
+
+function whatCanIDrink(age) {
+    if (age < 0 || age > 129) {
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
+    if (age < 14) {
+        return "Drink Toddy";
+    }
+    if (age < 18) {
+        return "Drink Coke";
+    }
+    if (age < 21) {
+        return "Drink Beer";
+    }
+    if (age < 130) {
+        return "Drink Whiskey";
+    }
+
+};
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,15 +1,47 @@
 describe("Calculator", function() {
+
+    beforeEach(function() {
+        calc = new Calculator;
+    });
+
     describe("Addition tests", function() {
         it("should return 42", function() {
-            expect(addition(20, 22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
         it("should return 26", function() {
-            expect(addition(7, 19)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
         it("should return an error if we don't supply two numbers", function() {
             spyOn(window, "alert");
-            addition("Hitchhikers", "Guide");
+            calc.add("Hitchhikers");
             expect(window.alert).toHaveBeenCalledWith("Error!");
+        });
+    });
+});
+
+describe("Drink About Game", function() {
+    describe("Age Checks", function() {
+        it("should return (130): Sorry. I can’t tell what drink because that age is incorrect!", function() {
+            expect(whatCanIDrink(130)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        it("should return (-1): Sorry. I can’t tell what drink because that age is incorrect!", function() {
+            expect(whatCanIDrink(-1)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        it("should return: Drink Toddy", function() {
+            expect(whatCanIDrink(13)).toBe("Drink Toddy");
+        });
+        it("should return: Drink Coke", function() {
+            expect(whatCanIDrink(17)).toBe("Drink Coke");
+        });
+        it("should return: Drink Beer", function() {
+            expect(whatCanIDrink(20)).toBe("Drink Beer");
+        });
+        it("should return: Drink Whiskey", function() {
+            expect(whatCanIDrink(129)).toBe("Drink Whiskey");
         });
     });
 });
